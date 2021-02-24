5a14b3fcb26a71451089ea2a3d4909c8bd26a1df
Fernando <flopezbarahona1998@gmail.com>
2021-02-24 16:42:54 -0600

    configuraciones

---
+++ configuracion/app.php
@@ -0,0 +1,6 @@
+<?php
+        const ServerUrl = "http://localhost:82/practica2021/MVC_DESTILERIA/";
+        const NombreSistema = "Sistema de Cotizaciones";
+        const Moneda = "L.";
+
+        date_default_timezone_set("America/Tegucigalpa");
---
+++ configuracion/servidor.php
@@ -0,0 +1,12 @@
+<?php
+
+        const Servidor = "localhost";
+        const db = "destileria";
+        const user = "root";
+        const contraseña = "";
+
+        const SGBD = "mysql:host=".servidor.";dbnombre=".db;
+
+        const METHOD = "AES-256-CBC";
+        const secretKey = '$destileria2021';
+        const secretId = '202102';
